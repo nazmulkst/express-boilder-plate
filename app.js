83d83f38b481c3b1153c1087b4e80f9686953f93
nazmulkst <nazmulkst@gmail.com>
2023-12-28 12:50:24 +0600

    Update App Index

--- app.js
+++ app.js
@@ -3,60 +3,78 @@
 // const router = require('./src/routes/api');
 // const app = new express();
 // const bodyParser = require('body-parser')
-import { Express } from 'express';
-import router from './src/route/api';
-import bodyParser from 'body-parser';
+// import { Express } from 'express';
+const express = require('express')
+const router = require('./src/route/api')
+const app = new express();
+const bodyParser = require('body-parser')
+// import router from './src/route/api';
+// import bodyParser from 'body-parser';
 
 
 // Security Middleware Library Import
-// const rateLimit = require('express-rate-limiter');
-import expressRateLimiter from 'express-rate-limiter';
-import helmet from 'helmet';
-import ExpressMongoSanitize from 'express-mongo-sanitize';
-import hpp from 'hpp';
-import { CorsOptions } from 'cors';
+const rateLimit = require('express-rate-limiter');
+const helmet = require('helmet');
+const ExpressMongoSanitize = require('express-mongo-sanitize')
+const hpp = require('hpp');
+const xss = require('xss-clean');
+const cors = require('cors');
+
+// import expressRateLimiter from 'express-rate-limiter';
+// import helmet from 'helmet';
+// import ExpressMongoSanitize from 'express-mongo-sanitize';
+// import hpp from 'hpp';
+// import { clean } from 'xss-clean/lib/xss';
+// import { CorsOptions } from 'cors';
 
 // Database Library Import
-import mongoose from 'mongoose';
+const mongoose = require('mongoose')
+// import mongoose from 'mongoose';
+// import { bodyParser } from 'body-parser';
+// import { helmet } from 'helmet';
+
 
 // Security Middleware Implement
 
-
-
-const app = new Express();
-
-app.use(CorsOptions());
+app.use(cors());
 app.use(helmet());
 app.use(ExpressMongoSanitize());
 app.use(hpp());
+app.use(xss())
 
 // Body Parser Implent
 app.use(bodyParser.json())
 
 // Request Rate Limit
-const limiter = expressRateLimiter({window: 15 * 60 * 1000, max: 3000});
-app.use(limiter);
+// const limiter = rateLimit({windowMs: 15 * 60 * 100, max: 3000});
+// app.use(limiter);
 
 
 
 // Mongo DB Database Connection
 
 const url = "mongodb://localhost:27017/inventory";
-const option = {autoIndex: true};
+// const option = {autoIndex: true};
 
-mongoose.connect(url, option, function(error){
-    console.log("Connection Successfully established")
-    console.log(error);
+// mongoose.connect(url, option, function(error){
+//     console.log("Connection Successfully established")
+//     console.log(error);
+// })
+
+
+// Frontend Tagging
+app.use(express.static('client/dist'));
+app.get("*", function(req, res){
+    req.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 })
 
-// Routing Implement
-
+// Managing Backend API Routing
 app.use("/api/v1", router);
 
 // Undefined Route Implement
 
-app.use("*", function(req, res){
-    res.status(404).json({status:"fail", data: "Not found"})
-})
+// app.use("*", function(req, res){
+//     res.status(404).json({status:"fail", data: "Not found"})
+// })
 
 module.exports = app;
---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+app = express();
+
+app.listen(5000, function(req, res){
+    console.log('Application Run Success')
+})
